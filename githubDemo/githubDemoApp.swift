be499dc76d4206bff6637fddec50ab216e9db4f4
Veronica Cheung <veronica.cheung1105@gmail.com>
2023-07-13 11:26:04 -0700

    Initial Commit

---
+++ githubDemo/githubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubDemoApp.swift
+//  githubDemo
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct githubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
